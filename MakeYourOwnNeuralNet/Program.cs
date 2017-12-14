4c937429098e1b01cf17498ad5eb257fa3eefd6b
Cristian Bidea <cbidea@amberstudio.ro>
2017-12-14 14:24:16 +0200

    Initial commit

---
+++ MakeYourOwnNeuralNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeYourOwnNeuralNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
