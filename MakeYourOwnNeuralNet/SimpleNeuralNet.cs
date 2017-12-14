7c2d9fea995f6e75cf4306ccf7b44f2c1a39353e
Cristian Bidea <cbidea@amberstudio.ro>
2017-12-14 16:37:00 +0200

    Initialization

--- MakeYourOwnNeuralNet/SimpleNeuralNet.cs
+++ MakeYourOwnNeuralNet/SimpleNeuralNet.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 
 namespace MakeYourOwnNeuralNet
 {
     class SimpleNeuralNet
     {
+        private int NumberOfInputNodes;
+        private int NumberOfHiddenNodes;
+        private int NumberOfOutputNodes;
+        private Matrix<double> W_input_hidden;
+        private Matrix<double> W_hiddhen_output;
+        private Random rndGen;
+
+        public SimpleNeuralNet(int NumberOfInputNodes, int NumberOfHiddenNodes, int NumberOfOutputNodes)
+        {
+            this.NumberOfInputNodes = NumberOfInputNodes;
+            this.NumberOfHiddenNodes = NumberOfHiddenNodes;
+            this.NumberOfOutputNodes = NumberOfOutputNodes;
+            rndGen = new Random();
+        }
+
+        public void Initialize()
+        {
+            W_input_hidden = DenseMatrix.Build.Dense(NumberOfInputNodes, NumberOfHiddenNodes);
+            W_hiddhen_output = DenseMatrix.Build.Dense(NumberOfHiddenNodes, NumberOfOutputNodes);
+            InitWeightsWithRandomValues();
+        }
+
+        private void InitWeightsWithRandomValues()
+        {
+            InitMatrixWithRandom(W_input_hidden);
+            InitMatrixWithRandom(W_hiddhen_output);
+        }
+
+        private void InitMatrixWithRandom(Matrix<double> mat)
+        {
+            var cols = mat.AsColumnMajorArray();
+            for (int i = 0; i < mat.ColumnCount * mat.RowCount; i++)
+            {
+               cols[i] = rndGen.NextDouble();
+            }
+        }
+
+        public void Train()
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("Initialize the network...");
+            var net = new SimpleNeuralNet(3, 3, 3);
+            net.Initialize();
         }
     }
 }
