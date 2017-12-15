147ab628b4785a7db8fff088ede0cbcbfcf0d1ab
Cristian Bidea <cbidea@amberstudio.ro>
2017-12-15 11:14:16 +0200

    Train function ready

--- MakeYourOwnNeuralNet/SimpleNeuralNet.cs
+++ MakeYourOwnNeuralNet/SimpleNeuralNet.cs
@@ -6,12 +6,16 @@ namespace MakeYourOwnNeuralNet
 {
     class SimpleNeuralNet
     {
+        public delegate double ActivationFunctionDelegate(double x);
         private int NumberOfInputNodes;
         private int NumberOfHiddenNodes;
         private int NumberOfOutputNodes;
         private Matrix<double> W_input_hidden;
         private Matrix<double> W_hiddhen_output;
         private Random rndGen;
+        
+        public ActivationFunctionDelegate ActivationFunction { set; get; }
+        public double LearningRate { set; get; }
 
         public SimpleNeuralNet(int NumberOfInputNodes, int NumberOfHiddenNodes, int NumberOfOutputNodes)
         {
@@ -19,6 +23,8 @@ namespace MakeYourOwnNeuralNet
             this.NumberOfHiddenNodes = NumberOfHiddenNodes;
             this.NumberOfOutputNodes = NumberOfOutputNodes;
             rndGen = new Random();
+            ActivationFunction = Sigmoid;
+            LearningRate = 0.3;
         }
 
         public void Initialize()
@@ -34,18 +40,43 @@ namespace MakeYourOwnNeuralNet
             InitMatrixWithRandom(W_hiddhen_output);
         }
 
+        public void Train(Vector<double> input, Vector<double> target)
+        {
+            var OWIH = W_input_hidden * input;
+            OWIH = OWIH.Map(x => ActivationFunction(x));
+            var OWHO = W_hiddhen_output * OWIH;
+            OWHO = OWHO.Map(x => ActivationFunction(x));
+
+            var output = OWHO;
+            var errors_output = target - output;
+            var errors_hidden = W_hiddhen_output.Transpose() * errors_output;
+
+            W_hiddhen_output += LearningRate * ((errors_output * output * (DenseVector.OfArray(new double[] { 1.0, 1.0, 1.0 }) - output)) * OWIH);
+            W_input_hidden += LearningRate * ((errors_hidden * OWIH * (DenseVector.OfArray(new double[] { 1.0, 1.0, 1.0 }) - OWIH)) * input);
+        }
+
+        public Vector<double> Query(Vector<double> input)
+        {
+            var OWIH = W_input_hidden * input;
+            OWIH = OWIH.Map(x => ActivationFunction(x));
+            var OWHO = W_hiddhen_output * OWIH;
+            OWHO = OWHO.Map(x => ActivationFunction(x));
+            return OWHO;
+        }
+
         private void InitMatrixWithRandom(Matrix<double> mat)
         {
             var cols = mat.AsColumnMajorArray();
             for (int i = 0; i < mat.ColumnCount * mat.RowCount; i++)
             {
-               cols[i] = rndGen.NextDouble();
+                cols[i] = rndGen.NextDouble() - 0.5;
             }
         }
 
-        public void Train()
+        private double Sigmoid(double x)
         {
-
+            double ex = Math.Pow(Math.E, x);
+            return ex / (ex + 1);
         }
 
         static void Main(string[] args)
@@ -53,6 +84,7 @@ namespace MakeYourOwnNeuralNet
             Console.WriteLine("Initialize the network...");
             var net = new SimpleNeuralNet(3, 3, 3);
             net.Initialize();
+            Console.WriteLine(net.Query(DenseVector.OfArray(new double[] { 1.0, 0.5, -1.5 })));
         }
     }
 }
